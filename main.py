c56efaae466be3fcef33d429b95e157705509ac9
isaacwach <isaacwachira57@gmail.com>
2024-03-22 13:11:08 +0300

    Modify the script to open my_file.txt in append mode to add 3 new lines

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ with open("my_file.txt", "w") as file:
     file.write("30\n")
     file.write("Successfuly finishing the module will require absolute dedication, worjing for more than 10 hours a day.\n")
 
-  # File reading and display
+# File reading and display
 with open("my_file.txt", "r") as file:
     content = file.read()
     print("The file contents are:\n")
     print(content)
+
+# File appending
+with open("my_file.txt", "a") as file:
+    file.write("Hello, hope you enjoye the module\n")
+    file.write("See you next week\n")
+    file.write("remember to complete the assignment\n")
