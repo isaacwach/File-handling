d2eec89c98267893d2e9fb3fa77b292fd747f9c1
isaacwach <isaacwachira57@gmail.com>
2024-03-22 13:07:28 +0300

    Enhance your script to read the contents of my_file.txt and display them on the console.

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ with open("my_file.txt", "w") as file:
     file.write("Welcome to file handling module\n")
     file.write("This is module 6\n")
     file.write("30\n")
+    file.write("Successfuly finishing the module will require absolute dedication, worjing for more than 10 hours a day.\n")
+
+  # File reading and display
+with open("my_file.txt", "r") as file:
+    content = file.read()
+    print("The file contents are:\n")
+    print(content)
